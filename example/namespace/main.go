6613baca787de4cf4df2cf24b36223e9fe1f664a
bearchit <bearchit@gmail.com>
2020-06-23 11:20:24 +0900

    fix namespace generator example

--- example/namespace/main.go
+++ example/namespace/main.go
@@ -8,11 +8,11 @@ import (
 
 func main() {
 	g1 := goid.NewNamespaceGenerator("com")
-	id1 := g1.Generate("github", "bearchit", "goid")
+	id1 := g1.Names("github", "bearchit", "goid").Generate()
 	fmt.Println(id1.String())
 
 	g2 := goid.NewNamespaceGenerator("com", goid.WithDelimiter("-"))
-	id2 := g2.Generate("github", "bearchit", "goid")
+	id2 := g2.Names("github", "bearchit", "goid").Generate()
 	fmt.Println(id2.String())
 
 	// Output
